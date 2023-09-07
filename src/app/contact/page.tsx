8d3dfbd26cb497d27cc9ee697a861c4c8e2a9351
Williams <williamsnkama@gmail.com>
2023-09-07 13:57:39 +0100

    Completed JSX

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,7 +10,67 @@ export default function Contact() {
 
     return (
         <div className="contact">
-            
+            <div className="main">
+                <div className="contact-container">
+                    <div className="contact">
+                        <h2>Contact Us</h2>
+
+                        <div className="contact-item">
+                            <Image alt="icon" src="/images/icons/mail.png" height={24} width={24}/>
+                            <p>Cybtracy@gmail.com</p>
+                        </div>
+
+                        <div className="contact-item">
+                            <Image alt="icon" src="/images/icons/phone.png" height={24} width={24}/>
+                            <p>+234 800 000 0000</p>
+                        </div>
+
+                        <div className="contact-item">
+                            <Image alt="icon" src="/images/icons/location.png" height={24} width={24}/>
+                            <p>124, Amadu Bello way, UNILAG, Lagos, Nigeria</p>
+                        </div>
+
+                        <div className="socials">
+                            <Link href="#" target="_blank">
+                                <Image alt="socials" src="/images/icons/fb-icon.png" height={30} width={30}/>
+                            </Link>
+
+                            <Link href="#" target="_blank">
+                                <Image alt="socials" src="/images/icons/x-icon.png" height={30} width={30}/>
+                            </Link>
+
+                            <Link href="#" target="_blank">
+                                <Image alt="socials" src="/images/icons/insta-icon.png" height={30} width={30}/>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="form-container">
+                    <form method="post">
+                        <h1>Get in Touch</h1>
+                        <p>We would love to hear from you.</p>
+
+                        <div className="input-group">
+                            <label htmlFor="fullname">Full Name *</label>
+                            <input id="fullname" type="text" name="fullname" required/>
+                        </div>
+
+                        <div className="input-group">
+                            <label htmlFor="email">Email *</label>
+                            <input id="email" type="text" name="email" required/>
+                            <p className="error-msg">Wrong Email format</p>
+                        </div>
+
+                        <div className="input-group">
+                            <label htmlFor="message">Message *</label>
+                            <textarea id="message" name="message" required/>
+                        </div>
+
+                        <button type="submit">Send message</button>
+                    </form>
+                </div>
+            </div>
             <HomeFooter/>
         </div>
     )
